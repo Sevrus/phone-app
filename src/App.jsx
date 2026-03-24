71c5419000707e3b9b84f4a63203e3edcc3316f3
Sevrus <book-n-troll@nordnet.fr>
2026-03-24 23:04:37 +0100

    Add `ConversationScreen` component and routing for displaying detailed SMS conversations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SmsListScreen from './pages/SmsListScreen';
+import ConversationScreen from './pages/ConversationScreen';
 
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/sms" element={<SmsListScreen />} />
+
+                <Route path="/sms/:contactId" element={<ConversationScreen />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;

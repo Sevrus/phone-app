27c6f3a332e5b7d0c640dec6b19e8e012cd148b8
Sevrus <book-n-troll@nordnet.fr>
2026-03-25 10:38:31 +0100

    Add `PinScreen` and `Header` components with modular CSS styling

--- src/pages/PinScreen.jsx
+++ src/pages/PinScreen.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import styles from "./PinScreen.module.css";
+
 export default function PinScreen() {
     const [pinInput, setPinInput] = useState("");
     const [attempts, setAttempts] = useState(0);
@@ -34,47 +36,50 @@ export default function PinScreen() {
     const remainingAttempts = MAX_ATTEMPTS - attempts;
 
     return (
-        <main>
-            <section className="main-pin">
-                { pinInput.length === 0 ? (
-                    <h2 className="main-pin__title">Entrez votre PIN</h2>
+        <main className={styles.mainContainer}>
+            <section className={styles.mainPin}>
+                {pinInput.length === 0 ? (
+                    <h2 className={styles.title}>Entrez votre PIN</h2>
                 ) : (
-                    <input type="password" value={pinInput} readOnly />
+                    <input id="enter-pin" className={styles.enterPin} type="password" value={pinInput} readOnly />
                 )}
 
-                <p className="main-pin__instructions">
+                <p className={styles.instructions}>
                     {attempts === 0
                         ? "Votre PIN contient 5 chiffres"
                         : `Il vous reste ${remainingAttempts} tentative${remainingAttempts > 1 ? "s" : ""}`}
                 </p>
 
-                <div className="main-pin__digits">
+                <div className={styles.digits}>
                     {[
-                        { num: "1", letters: "" }, { num: "2", letters: "ABC" }, { num: "3", letters: "DEF" },
-                        { num: "4", letters: "GHI" }, { num: "5", letters: "JKL" }, { num: "6", letters: "MNO" },
-                        { num: "7", letters: "PQRS" }, { num: "8", letters: "TUV" }, { num: "9", letters: "WXYZ" }
+                        {num: "1", letters: ""}, {num: "2", letters: "ABC"}, {num: "3", letters: "DEF"},
+                        {num: "4", letters: "GHI"}, {num: "5", letters: "JKL"}, {num: "6", letters: "MNO"},
+                        {num: "7", letters: "PQRS"}, {num: "8", letters: "TUV"}, {num: "9", letters: "WXYZ"}
                     ].map(key => (
                         <div
                             key={key.num}
-                            className="main-pin__digits__digit"
+                            className={styles.digit}
                             onClick={() => handleDigitClick(key.num)}
                         >
-                            <span className="main-pin__digits__digit--number">{key.num}</span>
-                            {key.letters && <span className="main-pin__digits__digit--letters">{key.letters}</span>}
+                            <span className={styles.digitNumber}>{key.num}</span>
+                            {key.letters && <span className={styles.digitLetters}>{key.letters}</span>}
                         </div>
                     ))}
 
-                    <div className="main-pin__digits__back" onClick={handleBackspace}>
-                        <img src="/assets/svg/backspace.svg" alt="Retour" />
+                    <div className={styles.backspace} onClick={handleBackspace}>
+                        <img src="/assets/svg/backspace.svg" alt="Retour"/>
                     </div>
 
-                    <div className="main-pin__digits__digit" onClick={() => handleDigitClick("0")}>
-                        <span className="main-pin__digits__digit--number">0</span>
+                    <div className={styles.digit} onClick={() => handleDigitClick("0")}>
+                        <span className={styles.digitNumber}>0</span>
                     </div>
 
-                    <div className="main-pin__digits__ok" onClick={handleOkClick}>
+                    <div className={styles.digitOk} onClick={handleOkClick}>
                         <span>OK</span>
                     </div>
+                </div>
+                <div>
+                    <img className={styles.empreinte} src="/assets/images/empreinte-digitale.png" alt="Une empreinte digitale"/>
                 </div>
             </section>
         </main>

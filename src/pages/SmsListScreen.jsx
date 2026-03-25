1d2b4b4187505c020f61efd835a513519e452f9e
Sevrus <book-n-troll@nordnet.fr>
2026-03-25 15:07:52 +0100

    Add modular CSS for CallListScreen and SmsListScreen, externalize data to JSON files

--- src/pages/SmsListScreen.jsx
+++ src/pages/SmsListScreen.jsx
@@ -2,28 +2,30 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import smsData from '../data/sms.json';
 
+import styles from './SmsListScreen.module.css';
+
 export default function SmsListScreen() {
     return (
         <main>
             <Header />
 
-            <h1 className="title">Messages</h1>
+            <h1 className={styles.title}>Messages</h1>
 
-            <section className="sms">
-                <div className="sms__container">
+            <section className={styles.sms}>
+                <div className={styles.container}>
                     {smsData.map((sms, index) => (
-                        <Link to={`/sms/${sms.htmlName}`} key={index} className="sms__container__item">
+                        <Link to={`/sms/${sms.htmlName}`} key={index} className={styles.item}>
 
-                            <div className="sms__container__item__avatar">
+                            <div className={styles.avatar}>
                                 <img src={sms.image.replace('../../', '/')} alt={`Avatar de ${sms.name}`} />
                             </div>
 
-                            <div className="sms__container__item__text">
+                            <div className={styles.text}>
                                 <h2>{sms.name}</h2>
                                 <p>{sms.message}</p>
                             </div>
 
-                            <div className="sms__container__item__date">
+                            <div className={styles.date}>
                                 <span>{sms.date}</span>
                             </div>
 
@@ -32,7 +34,7 @@ export default function SmsListScreen() {
                 </div>
             </section>
 
-            <footer className="footer">
+            <footer className={styles.footer}>
                 <Link to="/principal">
                     <img src="/assets/svg/arrow-back.svg" alt="Flèche retour" />
                 </Link>
